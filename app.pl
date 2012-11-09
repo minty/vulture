1ad2603bfcc1002f3d62ea19253dce981f446586
Murray <murray@minty.org>
2012-11-09 20:09:00 +0000

    Update todo lists in code

--- app.pl
+++ app.pl
@@ -38,6 +38,16 @@ plugin xslate_renderer => {
     }
 };
 
+# Goal:
+#   - via cmd line perl, make http request to morbo server to run a task
+#   - task js is defined in a git-committed file
+#   - js is run on all subscribed clients
+#
+# XXX Convert it into a proper Mojo class, with DBIx schemas
+# XXX Convert /get/task to be blocking
+# XXX Proxy fetch api
+# XXX simple key-value store api
+#
 # These return html pages.
 #   /page/client.html      # setup browser as a test client
 #   /page/edit.html        # edit a test
@@ -46,17 +56,6 @@ plugin xslate_renderer => {
 # The data should be written to a dir under git control
 #   /api/get/
 #   /api/set/
-#
-# These are json api hooks
-#   # Let a browser join/leave a test pool
-#   /api/client/join/
-#   /api/client/leave/
-#   /api/client/list/
-#
-#   # schedule a test to be run on all current clients
-#   /api/test/run/?test_file
-#   # for a client to report back test status
-#   /api/test/report/?key
 #
 # In due course we might also add reporting / stat pages
 #  /stats/blah...
